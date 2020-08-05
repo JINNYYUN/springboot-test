020d051ac6aa50d1a19a2d58774cebd454b7975f
YUNJIMYEONG <yjim3440@gmail.com>
2020-08-06 01:14:16 +0900

    게시글 등록, 조회

--- src/main/java/com/jinny/test/springboot/service/PostsService.java
+++ src/main/java/com/jinny/test/springboot/service/PostsService.java
@@ -1,12 +1,18 @@
 package com.jinny.test.springboot.service;
 
 
+import com.jinny.test.springboot.domain.posts.Posts;
 import com.jinny.test.springboot.domain.posts.PostsRepository;
+import com.jinny.test.springboot.web.dto.PostsListResponseDto;
+import com.jinny.test.springboot.web.dto.PostsResponseDto;
 import com.jinny.test.springboot.web.dto.PostsSaveRequestDto;
+import com.jinny.test.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -17,5 +23,30 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/jinny/test/springboot/web/indexController.java
@@ -0,0 +1,25 @@
+package com.jinny.test.springboot.web;
+
+import com.jinny.test.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class indexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
